057637e1b884d8793dd32c6514a92d026de9508f
Andrean Bimo <bimowibowo4846@gmail.com>
2025-01-16 09:42:00 +0700

    Basic Loop

---
+++ latihan1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        deret deret1 = new deret();
+
+        deret1.deret_biasa();
+
+        deret1.deret_3();
+    }
+}
---
+++ latihan1/src/deret.java
@@ -0,0 +1,15 @@
+public class deret {
+    public int n;
+
+    public void deret_biasa(){
+        for (n = 1; n <= 10; n++) {
+            System.out.println(n);
+        }
+    }
+
+    public void deret_3(){
+        for (n = 0;  n <= 10 ; n++) {
+            System.out.println(n*3);
+        }
+    }
+}
